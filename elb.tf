1e087688adf7eef7cd58c3c97893b98c244721d9
Areeb Ahsan <101670844+AreebAhsan@users.noreply.github.com>
2023-09-15 21:15:07 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,6 +1,6 @@
 resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
-  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+  name               = "areeb-terraform-elb"
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
 
   listener {
     instance_port     = 80
@@ -21,28 +21,32 @@ resource "aws_elb" "bar" {
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "areeb-tf-elb"
   }
 }
 
- /*resource "aws_rds_cluster" "default" {
+ resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
-  engine_version          = "5.7.mysql_aurora.2.11.2"
-  availability_zones      = ["ap-southeast-1a", "ap-southeast-1b"]
+  engine_version          = "5.7.mysql_aurora.3.02.0"
+  availability_zones      = ["ap-south-1a", "ap-south-1b"]
   database_name           = "mydb"
-  master_username         = "raham"
-  master_password         = "Rahamshaik#444555"
+  master_username         = "areeb"
+  master_password         = "Areeb#444555"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
-}*/
+}
 
-/*resource "aws_instance" "one" {
+resource "aws_instance" "one" {
   for_each = toset(["one", "two", "three"])
-  ami           = "ami-006be9ab6a140de6e"
+  ami           = "ami-0899663faf239dd8a"
   instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet[0].id
+  key_name = "ALKeyPair"
+  
+  availability_zone = "ap-south-1a"
   tags = {
     Name = "instance-${each.key}"
   }
 }
-*/
+
