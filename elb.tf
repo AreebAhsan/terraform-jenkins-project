4f9cdd1fc8f4b002e358ae2b4f701cfd64ad66b7
Areeb Ahsan <101670844+AreebAhsan@users.noreply.github.com>
2023-09-20 03:28:19 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,31 +1,64 @@
-resource "aws_elb" "bar" {
-  name               = "areeb-terraform-elb"
-  vpc_security_group_ids = ["${aws_security_group.five.id}"]
-  subnet_id = "${aws_subnet.public_subnet[0].id}"
-  availability_zones = ["ap-south-1a", "ap-south-1b"]
+# Create Application Load Balancer (ALB)
+resource "aws_lb" "my_alb" {
+  name               = "terra-alb"
+  internal           = false # Set to true if you want an internal ALB
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public_subnet[*].id
+  enable_deletion_protection = false # Set to true if you want deletion protection
+  security_groups    = [aws_security_group.five.id]
+}
 
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
+# Create a listener for the ALB (e.g., HTTP traffic on port 80)
+resource "aws_lb_listener" "my_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-  health_check {
-    healthy_threshold   = 3
-    unhealthy_threshold = 5
-    timeout             = 5
-    target              = "HTTP:80/"
-    interval            = 30
-  }
-
-  instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
-  cross_zone_load_balancing = true
-  idle_timeout              = 400
-  tags = {
-    Name = "areeb-tf-elb"
+  default_action {
+    type             = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "200"
+    }
   }
 }
+
+# Create a target group for the ALB
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "terra-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
+}
+
+# Attach instances from public subnets to the ALB
+resource "aws_lb_target_group_attachment" "public_instance_attachment_1" {
+  target_group_arn = aws_lb_target_group.my_target_group.arn
+  target_id        = aws_instance.one.id
+}
+
+resource "aws_lb_target_group_attachment" "public_instance_attachment_2" {
+  target_group_arn = aws_lb_target_group.my_target_group.arn
+  target_id        = aws_instance.two.id
+}
+
+
+# Associate the target group with the ALB listener
+resource "aws_lb_listener_rule" "my_listener_rule" {
+  listener_arn = aws_lb_listener.my_listener.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/"]
+    }
+  }
+}
+
 
  /*resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
